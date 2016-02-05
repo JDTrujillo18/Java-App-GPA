e34d869398cf669516dd2c942daa747afc299c4c
JDTrujillo18 <jrod1309@gmail.com>
2016-02-05 02:26:35 -0600

    Signed-off-by: JDTrujillo18 <jrod1309@gmail.com>

--- src/GPACalculator/Main.java
+++ src/GPACalculator/Main.java
@@ -5,6 +5,7 @@ import GPACalculator.frames.MainFrame;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello");
+		//Initiate Main Frame.
 		MainFrame mainFrame = new MainFrame();
 		mainFrame.showWelcomePanel();
 		mainFrame.makeVisible();
