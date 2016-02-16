cf5c1bbf787b5300171c24bc21523084c693b46d
JDTrujillo18 <jrod1309@gmail.com>
2016-02-15 21:54:11 -0600

    Added Apache POI libraries and test code to write to excel file
    Signed-off-by: JDTrujillo18 <jrod1309@gmail.com>

--- src/GPACalculator/Main.java
+++ src/GPACalculator/Main.java
@@ -6,14 +6,34 @@ import GPACalculator.student.Student;
 import java.awt.Dimension;
 import java.awt.List;
 import java.awt.Toolkit;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Date;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFDataFormat;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class Main {
 	public static void main(String[] args){
@@ -31,5 +51,46 @@ public class Main {
 		Student student = new Student("default");
 		student.setFirstName("Jered");
 		System.out.println(student.toString());
+		
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet("Sample sheet");
+		 
+		Map<String, Object[]> data = new HashMap<String, Object[]>();
+		data.put("1", new Object[] {"Emp No.", "Name", "Salary"});
+		data.put("2", new Object[] {1d, "John", 1500000d});
+		data.put("3", new Object[] {2d, "Sam", 800000d});
+		data.put("4", new Object[] {3d, "Dean", 700000d});
+		 
+		Set<String> keyset = data.keySet();
+		int rownum = 0;
+		for (String key : keyset) {
+		    Row row = sheet.createRow(rownum++);
+		    Object [] objArr = data.get(key);
+		    int cellnum = 0;
+		    for (Object obj : objArr) {
+		        Cell cell = row.createCell(cellnum++);
+		        if(obj instanceof Date) 
+		            cell.setCellValue((Date)obj);
+		        else if(obj instanceof Boolean)
+		            cell.setCellValue((Boolean)obj);
+		        else if(obj instanceof String)
+		            cell.setCellValue((String)obj);
+		        else if(obj instanceof Double)
+		            cell.setCellValue((Double)obj);
+		    }
+		}
+		 
+		try {
+		    FileOutputStream out = 
+		            new FileOutputStream(new File("test.xls"));
+		    workbook.write(out);
+		    out.close();
+		    System.out.println("Excel written successfully..");
+		     
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 	}
 }
