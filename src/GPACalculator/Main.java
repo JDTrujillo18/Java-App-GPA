5b679d39d774d221939b9467e6bc0e7a02b3400c
JDTrujillo18 <jrod1309@gmail.com>
2016-02-15 22:04:08 -0600

    Signed-off-by: JDTrujillo18 <jrod1309@gmail.com>

--- src/GPACalculator/Main.java
+++ src/GPACalculator/Main.java
@@ -1,39 +1,20 @@
 package GPACalculator;
-
 import GPACalculator.frames.MainFrame;
 import GPACalculator.student.Student;
-
 import java.awt.Dimension;
-import java.awt.List;
 import java.awt.Toolkit;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Date;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFDataFormat;
-import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.sl.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
 
 public class Main {
 	public static void main(String[] args){
